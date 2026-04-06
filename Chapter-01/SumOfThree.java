8128e3b82e52192836e8c287d9ee9b0019bb343f
Tushar Sharma <tusharsharmaji7532@gmail.com>
2026-04-06 21:47:42 +0530

    feat: add sum of three numbers program

---
+++ Chapter-01/SumOfThree.java
@@ -0,0 +1,11 @@
+public class Sumcode {
+
+    public static void main(String[] args ){
+        int num1= 3;
+        int num2= 6;
+        int num3= 8;
+        int sum =num1+num2+num3;
+        
+        System.out.println(sum);
+    }
+}
