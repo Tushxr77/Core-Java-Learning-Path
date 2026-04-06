14318e2997dfe24a3c86f678f25d9c1632acd6ae
Tushar Sharma <tusharsharmaji7532@gmail.com>
2026-04-07 00:01:34 +0530

    feat: add dynamic sum program input from userChapter 1 practice

---
+++ Chapter-01/InputFromUser.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Sumnondefinecode {
+    public static void main(String[] args){
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Three Numbers ");
+        int a, b, c;
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+        
+        int Sum = a+b+c;
+        System.out.println(Sum);
+        
+    }
+    
+}
