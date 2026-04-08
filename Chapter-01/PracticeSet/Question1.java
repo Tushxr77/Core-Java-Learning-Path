8e09983ece5879643f2793ae0440fc3b2baba6cb
Tushar Sharma <tusharsharmaji7532@gmail.com>
2026-04-09 00:07:02 +0530

    feat: add sum of two number

---
+++ Chapter-01/PracticeSet/Question1.java
@@ -0,0 +1,11 @@
+// Code with harry Question 1 of Practice set 1
+public class Question1 {
+    public static void main(String[] args) {
+        int num1=3;
+        int num2=7;
+        int sum=num1+num2;
+
+        System.out.println(sum);
+    }
+    
+}
