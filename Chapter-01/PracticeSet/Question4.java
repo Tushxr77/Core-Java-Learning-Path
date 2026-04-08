b8db1734d80284876a1e285e15ead89a548b050e
Tushar Sharma <tusharsharmaji7532@gmail.com>
2026-04-09 00:24:27 +0530

    feat: String Question

---
+++ Chapter-01/PracticeSet/Question4.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class question4 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter Your Name");
+        String str = sc.nextLine();
+    
+        
+
+        System.out.println("Hello "+str+",Have A Good day");
+    }
+    
+}
