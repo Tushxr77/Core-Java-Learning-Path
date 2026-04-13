f0f6b3077b70beb981d1f58e1f57d0eeba8ed8bc
Tushar Sharma <tusharsharmaji7532@gmail.com>
2026-04-13 22:37:52 +0530

    feat: solve Chapter 3 Practice Set - Question 2

---
+++ Chapter-03/Question2.java
@@ -0,0 +1,9 @@
+public class Question2 {
+    public static void main(String[] args) {
+        String name = "Tushar Sharma";
+
+        String replace = name.replace(" ","_");
+        System.out.println(replace);
+    }
+    
+}
