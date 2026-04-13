489ced94f105f57df2c865011807b54fc3bf5e13
Tushar Sharma <tusharsharmaji7532@gmail.com>
2026-04-13 22:52:23 +0530

    feat: solve Ch3 Question 5 - format letter using escape sequences
    Applied \n and \t escape sequences to format a raw string into a properly structured letter. This concludes the Chapter 3 practice set

---
+++ Chapter-03/Question5.java
@@ -0,0 +1,8 @@
+public class Question5 {
+  public static void main(String[] args) {
+    String letter= "Dear Harry,This Java Course Is Nice. Thanks ";
+
+    String letter2= "Dear Harry,\n\tThis Java Course Is Nice.\n Thanks ";
+    System.out.println(letter2);
+  }    
+}
