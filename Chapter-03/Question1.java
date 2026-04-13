faa9999b04b9f9f0ed5244e91654d4ae6627a443
Tushar Sharma <tusharsharmaji7532@gmail.com>
2026-04-13 22:30:29 +0530

    feat: solve Ch3 Question 1 - Convert string to lowercase
    Demonstrated the use of the .toLowerCase() method to transform a string literal into all lowercase characters

---
+++ Chapter-03/Question1.java
@@ -0,0 +1,9 @@
+public class Question1 {
+    public static void main(String[] args) {
+        String name = "Tushar Sharma";
+        String lowercase = name.toLowerCase();
+
+        System.out.println(lowercase);
+    }
+    
+}
